0654b4e13f9ee23489e3987e12c1fa159162816a
Joya322 <jannatferdoush1212@gmail.com>
2025-11-05 16:48:59 +0600

    sort by button functionality done

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -14,6 +14,8 @@ const ListedBooks = () => {
 
   const [wishListBooks, setWishListBooks] = useState([]);
 
+  const [sortBy, setSortBy] = useState("");
+
   useEffect(() => {
     // readList
     const storedReadList = getStoredList("read-list");
@@ -34,17 +36,82 @@ const ListedBooks = () => {
     setWishListBooks(wishList);
   }, []);
 
+  const handleSortBy = (sortType) => {
+    setSortBy(sortType);
+    // sortBy Rating
+    if (sortType === "Rating") {
+      // sortedReadListBooks
+      const sortedReadListBooks = [...readListBooks].sort(
+        (a, b) => b.rating - a.rating
+      );
+
+      setReadListBooks(sortedReadListBooks);
+
+      // sortedWishListBooks
+      const sortedWishListBooks = [...wishListBooks].sort(
+        (a, b) => b.rating - a.rating
+      );
+      setWishListBooks(sortedWishListBooks);
+    }
+
+    // sortBy Number of pages
+    else if (sortType === "Number of pages") {
+      // sortedReadListBooks
+      const sortedReadListBooks = [...readListBooks].sort(
+        (a, b) => b.totalPages - a.totalPages
+      );
+
+      setReadListBooks(sortedReadListBooks);
+
+      // sortedWishListBooks
+      const sortedWishListBooks = [...wishListBooks].sort(
+        (a, b) => b.totalPages - a.totalPages
+      );
+      setWishListBooks(sortedWishListBooks);
+    }
+
+    // sortBy Publisher year
+    else if (sortType === "Publisher year") {
+      // sortedReadListBooks
+      const sortedReadListBooks = [...readListBooks].sort(
+        (a, b) => b.yearOfPublishing - a.yearOfPublishing
+      );
+
+      setReadListBooks(sortedReadListBooks);
+
+      // sortedWishListBooks
+      const sortedWishListBooks = [...wishListBooks].sort(
+        (a, b) => b.yearOfPublishing - a.yearOfPublishing
+      );
+      setWishListBooks(sortedWishListBooks);
+    }
+  };
+
   return (
     <div className="pt-4 pb-8">
       <h2 className="text-3xl text-center py-5 bg-gray-200 rounded-2xl">
         Listed books
       </h2>
 
-      <div className="flex justify-center">
-        <button className="btn bg-[rgba(35,190,10,1)] px-4 text-white font-medium mt-6">
-          Sort By <IoIosArrowDown />
-        </button>
-      </div>
+      {/* dropdown button */}
+      <details className="dropdown flex justify-center">
+        <summary className="btn my-6 bg-[rgba(35,190,10,1)] text-white font-medium text-center pt-2">
+          {sortBy ? `Sort By: ${sortBy}` : "Sort By"}
+        </summary>
+        <ul className="menu dropdown-content bg-gray-200 rounded-box z-[1] w-52 p-2 shadow top-10 ml-3">
+          <li>
+            <a onClick={() => handleSortBy("Rating")}>Rating</a>
+          </li>
+          <li>
+            <a onClick={() => handleSortBy("Number of pages")}>
+              Number of pages
+            </a>
+          </li>
+          <li>
+            <a onClick={() => handleSortBy("Publisher year")}>Publisher year</a>
+          </li>
+        </ul>
+      </details>
 
       <Tabs>
         <TabList>
